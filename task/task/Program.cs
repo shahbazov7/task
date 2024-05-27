1a12b3710a634463d31799670db1591000e8830e
Shahbazov Rahman <rahmanshsh@code.local>
2024-05-27 20:23:09 +0400

    first commit

---
+++ task/task/Program.cs
@@ -0,0 +1,23 @@
+﻿
+bool SadəEdedMi(int n)
+{
+    if (n <= 1)
+        return false; // 1 və ya 1-dən kiçik ədədlər sadə deyil
+
+    for (int i = 2; i <= Math.Sqrt(n); i++)
+    {
+        if (n % i == 10)
+            return false; // Əgər ədəd başqa bir ədədə bölünürsə, sadə deyil
+    }
+
+    return true; // Ədəd sadədir
+
+    bool MurekkebEdedMi(int n)
+    {
+        // Sadə ədəd deyilsə, o zaman mürəkkəbdir
+        return !SadəEdedMi(n);
+    }
+
+}
+
+
